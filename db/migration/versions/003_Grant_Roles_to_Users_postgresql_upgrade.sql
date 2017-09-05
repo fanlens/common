d4cd494bb67530b604543e408cab520d228562ec
Christian Junker <chjdev@gmail.com>
2017-09-05 13:37:36 +0200

    small fix: add read user permission to worker

--- db/migration/versions/003_Grant_Roles_to_Users_postgresql_upgrade.sql
+++ db/migration/versions/003_Grant_Roles_to_Users_postgresql_upgrade.sql
@@ -1,6 +1,7 @@
 GRANT "manage.data" TO worker;
 GRANT "manage.meta" TO worker;
 GRANT "read.config" TO worker;
+GRANT "read.users" TO worker;
 GRANT "manage.data" TO web;
 GRANT "manage.users" TO web;
 GRANT "read.config" TO web;
